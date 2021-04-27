fee944cf8942328c93a06fe27b11377015e4000f
BackRunner <dev@backrunner.top>
2021-04-27 13:59:07 +0800

    fix: rollup bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,50 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import vue from 'rollup-plugin-vue';
-import postcss from 'rollup-plugin-postcss';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import alias from '@rollup/plugin-alias';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: './index.js',
   output: {
     format: 'esm',
     file: 'dist/vue-titlebar.js',
-    exports: 'default',
+    exports: 'named',
   },
+  external: ['vue'],
   plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      preventAssignment: true,
+    }),
+    alias({
+      entries: [
+        {
+          find: '@',
+          replacement: __dirname,
+        },
+      ],
+    }),
     vue({
       css: true,
+      target: 'browser',
       include: /\.vue$/,
+      template: {
+        isProduction: true,
+      },
     }),
-    resolve(),
-    postcss({
-      inject: true,
+    resolve({
+      extensions: ['.js', '.vue'],
     }),
+    commonjs(),
     babel({
-      babelHelpers: 'runtime',
-      exclude: '**/node_modules/**',
+      babelHelpers: 'bundled',
+      extensions: ['.js', '.vue'],
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env'],
     }),
+    terser(),
   ],
 };
